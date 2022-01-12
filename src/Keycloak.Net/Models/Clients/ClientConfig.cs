833e06773dc0540e7a2dc5481e962058be647024
tung-lin <im_fish@msn.com>
2022-01-13 07:46:28 +0800

    Update ClientConfig model

--- src/Keycloak.Net/Models/Clients/ClientConfig.cs
+++ src/Keycloak.Net/Models/Clients/ClientConfig.cs
@@ -4,7 +4,7 @@ namespace Keycloak.Net.Models.Clients
 {
     public class ClientConfig
     {
-        [JsonProperty("userinfotokenclaim")]
+        [JsonProperty("userinfo.token.claim")]
         public string UserInfoTokenClaim { get; set; }
         [JsonProperty("userattribute")]
         public string UserAttribute { get; set; }
@@ -14,6 +14,8 @@ namespace Keycloak.Net.Models.Clients
         public string AccessTokenClaim { get; set; }
         [JsonProperty("claim.name")]
         public string ClaimName { get; set; }
+        [JsonProperty("claim.value")]
+        public string ClaimValue { get; set; }
         [JsonProperty("jsonType.label")]
         public string JsonTypelabel { get; set; }
         [JsonProperty("user.session.note")]
